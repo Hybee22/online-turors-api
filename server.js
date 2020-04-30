0e03168b7e6d02493af0bc697ca746a63efedb84
Ibrahim Adekunle <adefemi101@gmail.com>
2020-04-30 09:09:20 +0100

    updated to access the app module

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 
 dotenv.config({ path: './config.env'})
 
+const app = require('./app');
+
 // DATABASE URL STRING
 const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD)
 
@@ -21,4 +23,4 @@ mongoose
 
 // START SERVER
 const port = process.env.PORT || 5000;
-    
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));   
